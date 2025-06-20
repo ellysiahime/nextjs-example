7f396ef7d59e0da209008c7b349b961862564dcd
ellysiahime <ellysiahime@gmail.com>
2025-06-20 04:06:53 +0000

    product list page

---
+++ ecommerce-site/app/cart/page.tsx
@@ -0,0 +1,8 @@
+export default function CartPage() {
+  return (
+    <main>
+      <h1>Shopping Cart</h1>
+      {/* Add your content here */}
+    </main>
+  );
+}
---
+++ ecommerce-site/app/checkout/page.tsx
@@ -0,0 +1,8 @@
+export default function CheckoutPage() {
+  return (
+    <main>
+      <h1>Check Out</h1>
+      {/* Add your content here */}
+    </main>
+  );
+}
