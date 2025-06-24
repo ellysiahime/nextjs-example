5a30e6b52479c7acf56924ba40622804309b8a49
ellysiahime <ellysiahime@gmail.com>
2025-06-24 05:04:59 +0000

    navbar, products api, shopping cart api

--- ecommerce-site/app/checkout/page.tsx
+++ ecommerce-site/app/checkout/page.tsx
@@ -1,8 +1,8 @@
 export default function CheckoutPage() {
   return (
-    <main>
-      <h1>Check Out</h1>
-      {/* Add your content here */}
-    </main>
+    <div className="container mx-auto p-8 flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold mb-4">Check Out</h1>
+      <p className="text-gray-600 text-center">This feature is coming soon!</p>
+    </div>
   );
 }
